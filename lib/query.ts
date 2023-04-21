f03a473f6e92eaca2923929b1ed83ab8b96b52f9
Nabarun <nicknabarun@gmail.com>
2023-04-21 18:53:18 +0530

    Minor fix

--- lib/query.ts
+++ lib/query.ts
@@ -8,7 +8,7 @@ export async function query(prompt: string, id: string, model: string) {
       user: id,
       temperature: 0.9,
       top_p: 1,
-      max_tokens: 3000,
+      max_tokens: 5000,
       frequency_penalty: 1,
       presence_penalty: 1,
     })
