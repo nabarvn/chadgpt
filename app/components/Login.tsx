63eb80cb9dcb930da5364a031ef981f787b19457
Nabarun <nicknabarun@gmail.com>
2023-05-01 18:26:43 +0530

    Styles fix

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -15,12 +15,12 @@ const Login = () => {
         width={100}
         height={100}
         alt='ChadGPT Logo'
-        className='h-96 w-auto lg:h-52 lg:w-72'
+        className='h-96 w-auto lg:h-40 lg:w-auto xl:h-52 xl:w-72'
         priority
       />
 
       <div className='m-9'>
-        <p className='text-9xl lg:text-5xl text-gray-700 font-extrabold'>
+        <p className='text-9xl lg:text-3xl xl:text-5xl text-gray-700 font-extrabold'>
           ChadGPT
         </p>
         <p className='text-3xl lg:text-base text-gray-700 font-bold'>
@@ -38,10 +38,10 @@ const Login = () => {
           width={100}
           height={100}
           alt='Google Logo'
-          className='h-20 w-20 lg:h-9 lg:w-9'
+          className='h-20 w-20 lg:h-6 lg:w-6 xl:h-9 xl:w-9'
           priority
         />
-        <p className='text-[#757575] text-5xl lg:text-2xl font-semibold pr-2 lg:pr-1.5'>
+        <p className='text-[#757575] text-5xl lg:text-base xl:text-2xl font-semibold pr-2 lg:pr-1.5'>
           Sign in with Google
         </p>
       </button>
