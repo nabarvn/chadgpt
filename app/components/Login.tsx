eee0a0146f1d6e8b5ec049316873d036d96bedd7
Nabarun <nicknabarun@gmail.com>
2023-05-01 17:46:21 +0530

    Add sign-in button

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 
 const Login = () => {
   return (
-    <div className='flex flex-col items-center justify-center text-center bg-[#11A37F] min-h-screen'>
+    <div
+      className='flex flex-col items-center justify-center text-center bg-[#11A37F]'
+      style={{ height: "100svh" }}
+    >
       <Image
         unoptimized
         src='https://links.papareact.com/2i6'
@@ -16,11 +19,29 @@ const Login = () => {
         priority
       />
 
+      <div className='m-9'>
+        <p className='text-5xl text-gray-700 font-extrabold'>ChadGPT</p>
+        <p className='text-base text-gray-700 font-bold'>
+          (This app may or may not be notable)
+        </p>
+      </div>
+
       <button
         onClick={() => signIn("google", { callbackUrl: "/" })}
-        className='text-white text-3xl font-bold'
+        className='flex space-x-3 bg-white rounded-lg hover:shadow-lg active:bg-gray-300 p-3'
       >
-        Sign In to use ChadGPT
+        <Image
+          unoptimized
+          src='./google.png'
+          width={100}
+          height={100}
+          alt='Google Logo'
+          className='h-9 w-9'
+          priority
+        />
+        <p className='text-[#757575] text-2xl font-semibold pr-1.5'>
+          Sign in with Google
+        </p>
       </button>
     </div>
   );
