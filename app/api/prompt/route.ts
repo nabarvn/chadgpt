3da50492a1a95a9b52703c5b62ffefe708a48f46
Nabarun <nicknabarun@gmail.com>
2023-05-25 16:50:38 +0530

    Implement chat context

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -3,7 +3,7 @@ import { adminDb } from "@/firebase/firebaseAdmin";
 import { query } from "@/lib/query";
 
 export async function POST(request: Request) {
-  const { prompt, id, model, session } = await request.json();
+  const { prompt, outboundMessages, id, model, session } = await request.json();
 
   if (!prompt) {
     return new Response(
@@ -24,7 +24,7 @@ export async function POST(request: Request) {
   }
 
   // ChatGPT Query
-  const response = await query(prompt, id, model);
+  const response = await query(outboundMessages, id, model);
 
   const message: Message = {
     text: response || "Chad could not find a response",
