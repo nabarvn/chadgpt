954b036abe7e055da52bd1678c1cf98238bef700
Nabarun <64539836+nabarvn@users.noreply.github.com>
2025-10-14 15:24:30 -0400

    Set up Firebase admin SDK initialization for server-side use

---
+++ src/firebase/server.ts
@@ -0,0 +1,19 @@
+import admin from "firebase-admin";
+import { getApps } from "firebase-admin/app";
+
+if (!getApps().length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(
+        /\\n/g,
+        "\n",
+      ),
+    }),
+  });
+}
+
+const adminDb = admin.firestore();
+
+export { adminDb };
